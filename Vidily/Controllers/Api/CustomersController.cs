22813ea61b215721d4b367fb654816b5408811d4
Robert Higginbotham <higrob2004@gmail.com>
2018-05-17 16:03:10 +0100

    API AutoMapper

--- Vidily/Controllers/Api/CustomersController.cs
+++ Vidily/Controllers/Api/CustomersController.cs
@@ -4,10 +4,83 @@ using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Web.Http;
+using AutoMapper;
+using Vidily.DTOs;
+using Vidily.Models;
 
 namespace Vidily.Controllers.Api
 {
     public class CustomersController : ApiController
     {
+
+        private ApplicationDbContext _context;
+
+        public CustomersController()
+        {
+            _context = new ApplicationDbContext();
+        }
+        //Get  /api/customers
+        public IEnumerable<CustomerDto> GetCustomers()
+        {
+            return _context.Customers.ToList().Select(Mapper.Map<Customer,CustomerDto>);
+        }
+
+
+        public CustomerDto GetCustomer(int id)
+        {
+            var customer = _context.Customers.SingleOrDefault(c => c.Id == id);
+            if (customer ==null)
+                throw new HttpResponseException(HttpStatusCode.NotFound);
+            return Mapper.Map<Customer,CustomerDto>(customer);
+        }
+
+
+        //post /api/Customer
+        [HttpPost]
+        public CustomerDto CreateCustomer(CustomerDto customerDto)
+        {
+            if (!ModelState.IsValid)
+                throw new HttpResponseException(HttpStatusCode.BadRequest);
+
+            var customer = Mapper.Map<CustomerDto, Customer>(customerDto);
+            _context.Customers.Add(customer);
+            _context.SaveChanges();
+
+            customerDto.Id = customer.Id;
+            return customerDto;
+        }
+
+
+        //put /api/customerDto/1
+        [HttpPut]
+        public void UpdateCustomer(int id, CustomerDto customerDto)
+        {
+            if (!ModelState.IsValid)
+                throw new HttpResponseException(HttpStatusCode.BadRequest);
+
+
+            var customerInDb = _context.Customers.Single(c => c.Id == id);
+            if (customerInDb == null)
+                throw new HttpResponseException(HttpStatusCode.NotFound);
+
+            Mapper.Map(customerDto,customerInDb);
+           
+
+            _context.SaveChanges();
+        }
+
+        // Delete  /api/customerDto/1
+        [HttpDelete]
+        public void DeleteCustomer(int id, CustomerDto customer)
+        {
+            if (!ModelState.IsValid)
+                throw new HttpResponseException(HttpStatusCode.BadRequest);
+
+            var customerInDb = _context.Customers.Single(c => c.Id == id);
+            if (customerInDb == null)
+                throw new HttpResponseException(HttpStatusCode.NotFound);
+            _context.Customers.Remove(customerInDb);
+            _context.SaveChanges();
+        }
     }
 }
---
+++ Vidily/DTOs/CustomerDTO.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+using Vidily.Models;
+
+namespace Vidily.DTOs
+{
+    public class CustomerDto
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        public bool IsSubscribedToNewsLetter { get; set; }
+
+       
+        public byte MembershipTypeId { get; set; }
+
+      
+        [Min18YearsIfAMember]
+        public DateTime? DateOfBirth { get; set; }
+
+    }
+}
